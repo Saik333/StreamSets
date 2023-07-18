024f9cfd4bce822b8bbbef4ec8001f823abdb1cb
Saik333 <sai.rithwik34@gmail.com>
2023-07-18 12:58:01 +0530

    test

--- SS.py
+++ SS.py
@@ -11,7 +11,7 @@ try:
     logging.info("Connection to controlHub is Successful")
 except Exception as e:
     logging.info("Connection failed")
-    # raise
+    raise
 
 # sdc = sch.data_collectors.get(url='https://eu01.hub.streamsets.com/sch/jobRunner/engines/dataCollectors/0f93a683-1d26-4a35-906e-b4402bc2c85e')
 pipeline_builder = sch.get_pipeline_builder(engine_type='data_collector', engine_id='0f93a683-1d26-4a35-906e-b4402bc2c85e')
@@ -32,15 +32,15 @@ trash = pipeline_builder.add_stage('Trash')
 dev_raw_data_source >> trash
 
 #create pipeline
-pipeline = pipeline_builder.build('SDK_TEST')
+pipeline = pipeline_builder.build('SDK_TEST_11')
 logging.info("publishing new pipeline")
-sch.publish_pipeline(pipeline, commit_message='SDK_TEST')
+sch.publish_pipeline(pipeline, commit_message='SDK_TEST_11')
 
 #create job
 logging.info("creating job for the pipeline")
 job_builder = sch.get_job_builder()
-pipeline = sch.pipelines.get(name='SDK_TEST')
-job = job_builder.build('job_My first pipeline from SDK', pipeline=pipeline, runtime_parameters={'name':"NA"})
+pipeline = sch.pipelines.get(name='SDK_TEST_11')
+job = job_builder.build('job_My first pipeline from SDK TEST11', pipeline=pipeline, runtime_parameters={'name':"NA"})
 job.data_collector_labels=['streamsetstraining_dep']
 sch.add_job(job)
 logging.info("Process executed successfully")
